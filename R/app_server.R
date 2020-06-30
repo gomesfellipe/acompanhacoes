23a37f9d77ccecf4ecef8b0ac70e0665c90b9818
gomesfellipe <gomes.fellipe@hotmail.com>
2020-06-29 21:00:03 -0300

    incluir data da tabela

--- R/app_server.R
+++ R/app_server.R
@@ -1,135 +1,164 @@
 #' The application server-side
-#' 
-#' @param input,output,session Internal parameters for {shiny}. 
+#'
+#' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
-app_server <- function( input, output, session ) {
-  
+app_server <- function(input, output, session) {
+
   # Leitura dos dados -------------------------------------------------------
-  
+
   portifolio <- reactive({
-    
-    read.csv(input$portifolio_file$datapath, stringsAsFactors = F) %>% 
-      as_tibble() %>% 
-      mutate_all(~ .x %>% str_trim() %>% str_squish()) %>% 
-      mutate_at(c("cot_ini", "qtd"), as.numeric)  
-    
+    read.csv(input$portifolio_file$datapath, stringsAsFactors = F) %>%
+      as_tibble() %>%
+      mutate_all(~ .x %>%
+        str_trim() %>%
+        str_squish()) %>%
+      mutate_at(c("cot_ini", "qtd"), as.numeric)
   })
-  
-  output$selecionar_stock <-  renderUI({
+
+  output$selecionar_stock <- renderUI({
     req(input$portifolio_file$datapath)
-    selectInput('stock', 'Stocks', unique(portifolio()$symbol))
+    selectInput("stock", "Stocks", unique(portifolio()$symbol))
   })
-  
-  
+
+
   # Requisicao  -------------------------------------------------------------
-  
+
   stocks <-
     reactive({
       req(input$portifolio_file)
-      
-      first_day_year <- Sys.Date() %>% `day<-`(1) %>% `month<-`(1)
-      
-      request <- tryCatch({
-        map_df(unique(portifolio()$symbol),
-               ~tq_get(.x, get = "stock.prices", from = first_day_year))  
-      }, error = function(e){
-        Sys.sleep(2)
-        map_df(unique(portifolio()$symbol),
-               ~tq_get(.x, get = "stock.prices", from = first_day_year))  
-      })
-      
+
+      first_day_year <- Sys.Date() %>%
+        `day<-`(1) %>%
+        `month<-`(1)
+
+      request <- tryCatch(
+        {
+          map_df(
+            unique(portifolio()$symbol),
+            ~ tq_get(.x, get = "stock.prices", from = first_day_year)
+          )
+        },
+        error = function(e) {
+          Sys.sleep(2)
+          map_df(
+            unique(portifolio()$symbol),
+            ~ tq_get(.x, get = "stock.prices", from = first_day_year)
+          )
+        }
+      )
     })
-  
+
   # Selecionar acao ---------------------------------------------------------
-  
+
   output$plot1 <- renderHighchart({
     req(input$portifolio_file)
-    
-    stocks() %>% 
+
+    stocks() %>%
       filter(symbol == input$stock) %>%
-      timetk::tk_xts(date_var = date) %>% 
-      highcharter::hchart() 
-    
+      timetk::tk_xts(date_var = date) %>%
+      highcharter::hchart()
   })
-  
+
   # Tabela financeira -------------------------------------------------------
-  
+
   # converter para tsibble
   tbl_stocks <- reactive({
     req(input$portifolio_file)
-    
+
     stocks() %>%
-      as_tsibble(key = symbol, index = date) %>% 
-      fill_gaps() %>% 
+      as_tsibble(key = symbol, index = date) %>%
+      fill_gaps() %>%
       tidyr::fill(c(open, high, low, close, volume, adjusted),
-                  .direction = "down") 
+        .direction = "down"
+      )
   })
-  
+
   # obter dados do ultimo dia
-  fechamento_ultimo_dia <- 
+  fechamento_ultimo_dia <-
     reactive({
       req(input$portifolio_file)
-      
-      tbl_stocks() %>% 
-        filter(date == case_when(wday(Sys.Date()) == 7 ~ Sys.Date()-1,
-                                 wday(Sys.Date()) == 1 ~ Sys.Date()-2,
-                                 wday(Sys.Date()) == 2 ~ Sys.Date()-3,
-                                 T ~ Sys.Date() - 1)) %>% 
+
+      tbl_stocks() %>%
+        filter(date == case_when(
+          wday(Sys.Date()) == 7 ~ Sys.Date() - 1,
+          wday(Sys.Date()) == 1 ~ Sys.Date() - 2,
+          wday(Sys.Date()) == 2 ~ Sys.Date() - 3,
+          T ~ Sys.Date() - 1
+        )) %>%
         select(symbol, cot_atual = close)
-      
     })
-  
+
+  # print data do dia
+  output$ultimo_dia <- renderText({
+    format(as.Date(fechamento_ultimo_dia()$date[1]) %>%
+      format("%d/%m/%Y"))
+  })
+
   # Criar tabela financeita
-  tab_financeira <- 
+  tab_financeira <-
     reactive({
-      left_join(portifolio(), fechamento_ultimo_dia()) %>% 
-        select(date, everything()) %>% 
+      left_join(portifolio(), fechamento_ultimo_dia()) %>%
+        select(date, everything()) %>%
         mutate(cot_atual = if_else(symbol == "BTC-USD",
-                                   cot_atual * quantmod::getQuote("USDBRL=X")[1,"Open"], 
-                                   cot_atual) ) %>% 
-        mutate(vol_ini = cot_ini * qtd,
-               vol_atual = cot_atual * qtd,
-               ganho_perda = vol_atual - vol_ini,
-               resultado_bruto = round(ganho_perda / vol_ini * 100, 2)) %>% 
-        select(symbol, cot_ini, qtd, vol_ini, cot_atual, qtd, vol_atual, 
-               ganho_perda, resultado_bruto) 
-      
+          cot_atual * quantmod::getQuote("USDBRL=X")[1, "Open"],
+          cot_atual
+        )) %>%
+        mutate(
+          vol_ini = cot_ini * qtd,
+          vol_atual = cot_atual * qtd,
+          ganho_perda = vol_atual - vol_ini,
+          resultado_bruto = round(ganho_perda / vol_ini * 100, 2)
+        ) %>%
+        select(
+          symbol, cot_ini, qtd, vol_ini, cot_atual, qtd, vol_atual,
+          ganho_perda, resultado_bruto
+        )
     })
-  
+
   # Tabela financeira
-  output$tab_financeira <- function(){
+  output$tab_financeira <- function() {
     req(input$portifolio_file)
-    
+
     tab_financeira() %>%
-      mutate_all(~ifelse(is.na(.x), 0, .x)) %>% 
+      mutate_all(~ ifelse(is.na(.x), 0, .x)) %>%
       mutate(
         cot_ini = moeda_real(cot_ini),
         cot_atual = moeda_real(cot_atual),
         vol_ini = moeda_real(vol_ini),
         vol_atual = moeda_real(vol_atual),
-        qtd = round(qtd,4),
-        ` ` = ifelse(ganho_perda > 0,"\u2713", "\u2718") ,
+        qtd = round(qtd, 4),
+        ` ` = ifelse(ganho_perda > 0, "\u2713", "\u2718"),
         cot_atual = cell_spec(cot_atual, "html", color = "blue"),
         ganho_perda = cell_spec(moeda_real(ganho_perda), "html",
-                                color = ifelse(ganho_perda > 0, 
-                                               "green", "red")),
+          color = ifelse(ganho_perda > 0,
+            "green", "red"
+          )
+        ),
         resultado_bruto = cell_spec(porcentagem(resultado_bruto), "html",
-                                    color = ifelse(resultado_bruto > 0, 
-                                                   "green", "red"))) %>% 
-      `colnames<-`(c("Ativo", "Cotação inicio", "Quantidade",
-                     "Volume Inicio", "Cotação atual", "Voluma atual",
-                     "Ganho/Perda", "Resutado Bruto", "Status")) %>% 
+          color = ifelse(resultado_bruto > 0,
+            "green", "red"
+          )
+        )
+      ) %>%
+      `colnames<-`(c(
+        "Ativo", "Cotação inicio", "Quantidade",
+        "Volume Inicio", "Cotação atual", "Voluma atual",
+        "Ganho/Perda", "Resutado Bruto", "Status"
+      )) %>%
       kable(format = "html", escape = F) %>%
-      kable_styling(c("bordered", "hover", "responsive"), 
-                    full_width = T, font_size = 12) %>%
-      add_header_above(c(" ", "Montagem" = 3,
-                         "Desmontagem / Atual" = 2, "Resultado" = 3))
+      kable_styling(c("bordered", "hover", "responsive"),
+        full_width = T, font_size = 12
+      ) %>%
+      add_header_above(c(" ",
+        "Montagem" = 3,
+        "Desmontagem / Atual" = 2, "Resultado" = 3
+      ))
   }
-  
+
   # Download exemplo de input -----------------------------------------------
-  
+
   output$input_test <- downloadHandler(
     filename = function() {
       "input_test.txt"
@@ -138,5 +167,4 @@ app_server <- function( input, output, session ) {
       input_exemplo %>% write.csv(con, row.names = F)
     }
   )
-  
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -1,6 +1,6 @@
 #' The application User-Interface
-#' 
-#' @param request Internal parameter for `{shiny}`. 
+#'
+#' @param request Internal parameter for `{shiny}`.
 # Sys.setenv(TZ='UTC')
 Sys.setenv(TZ = "America/Sao_Paulo")
 #'     DO NOT REMOVE.
@@ -10,49 +10,48 @@ app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
-    # List the first level UI elements here 
+    # List the first level UI elements here
     pageWithSidebar(
-      headerPanel('Acompanhamento de Ações'),
+      headerPanel("Acompanhamento de Ações"),
       sidebarPanel(
         fileInput("portifolio_file", "Insira os dados do portifólio aqui"),
         uiOutput("selecionar_stock"),
         tags$b("Ajuda:"), downloadLink("input_test", "Exemplo de input"), br(),
-        tags$p(tags$b("Código Fonte: "), tags$a(href = "https://github.com/gomesfellipe/acompanhacoes",
-                                      "github.com/gomesfellipe/acompanhacoes")) 
-        
+        tags$p(tags$b("Código Fonte: "), tags$a(
+          href = "https://github.com/gomesfellipe/acompanhacoes",
+          "github.com/gomesfellipe/acompanhacoes"
+        ))
       ),
       mainPanel(
-        h2("Série Histórica:"),br(),
-        highchartOutput('plot1')%>% withSpinner(), br(),
-        h2("Tabela Financeira"),
-        tableOutput("tab_financeira")%>% withSpinner()
+        h2("Série Histórica:"), br(),
+        highchartOutput("plot1") %>% withSpinner(), br(),
+        fluidRow(h2("Tabela Financeira"), textOutput("ultimo_dia")), br(),
+        tableOutput("tab_financeira") %>% withSpinner()
       )
     )
   )
 }
 
 #' Add external Resources to the Application
-#' 
-#' This function is internally used to add external 
-#' resources inside the Shiny application. 
-#' 
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
 #' @import shiny
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
-golem_add_external_resources <- function(){
-  
+golem_add_external_resources <- function() {
   add_resource_path(
-    'www', app_sys('app/www')
+    "www", app_sys("app/www")
   )
- 
+
   tags$head(
     favicon(),
     bundle_resources(
-      path = app_sys('app/www'),
-      app_title = 'acompanhacoes'
+      path = app_sys("app/www"),
+      app_title = "acompanhacoes"
     )
     # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert() 
+    # for example, you can add shinyalert::useShinyalert()
   )
 }
-
